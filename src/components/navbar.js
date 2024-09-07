5122a61a2eedb77079c79b0581e3cab75b88e1f3
Fredaobob <lokgamer01@gmail.com>
2024-09-07 12:47:20 -0300

    feat: started working on responsiviness

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,21 +1,39 @@
+import { useState } from 'react';
 import '../styles/navbar.scss';
 
 const NavbarComponent = () => {
-  return (  
-    <header className='navbarHeader'>
-      <div className='navbarHeaderMainWrapper'>
-        <div className='navbarHeaderLogo'>
-          <a href='#'><code>ale.dev</code></a>
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <header className="navbarHeader">
+      <div className="navbarHeaderMainWrapper">
+        <div className="navbarHeaderLogo">
+          <a href="#"><code>ale.dev</code></a>
         </div>
-        <nav className='navbarHeaderNav'>
+        <nav className="navbarHeaderNav">
           <a href="#home">Home</a>
           <a href="#projects">Projects</a>
           <a href="#experience">Experience</a>
           <a href="#contact">Contact</a>
         </nav>
+        <div className="hamburgerMenu" onClick={toggleMenu}>
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+      </div>
+      <div className={`dropdownMenu ${isOpen ? 'active' : ''}`}>
+        <a href="#home" onClick={toggleMenu}>Home</a>
+        <a href="#projects" onClick={toggleMenu}>Projects</a>
+        <a href="#experience" onClick={toggleMenu}>Experience</a>
+        <a href="#contact" onClick={toggleMenu}>Contact</a>
       </div>
     </header>
   );
-}
+};
 
 export default NavbarComponent;
